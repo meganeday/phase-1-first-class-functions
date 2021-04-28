0feda6bdc3b69a865c2afcde4dacc4d90e671dcc
Megan Day <meganeday7@gmail.com>
2021-04-28 13:38:31 -0500

    commit tree da44f250ca0429bed266cdfa3021bd84f9ecc466

---
+++ index.js
@@ -0,0 +1,10 @@
+function receivesAFunction(callback) {
+    var fn = returnsANamedFunction() {
+        name: Megan
+    }
+    var fn = receivesAnAnonymousFunction() {
+        return function() {};
+    }
+}
+
+receivesAFunction(spy);
